95f9b503525a5ae8b698b494901bca5bc5fe07d6
Tui <146558116+arthuurtm@users.noreply.github.com>
2025-02-10 21:59:32 -0300

    big update 1

--- js/formModel.js
+++ js/formModel.js
@@ -1,58 +1,70 @@
 // script.js
 document.addEventListener('DOMContentLoaded', () => {
-  let currentStep = 1;
-  const totalSteps = document.querySelectorAll('.formStep').length;
-  console.log('leght: ', totalSteps)
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const type = urlParams.get('type');
+    let currentStep = 1;
+    const totalSteps = document.querySelectorAll('.formStep').length;
+    console.log('leght: ', totalSteps)
 
-  const formSteps = Array.from(document.querySelectorAll('.formStep'));
-  const prevButton = document.getElementById('prevButton');
-  const nextButton = document.getElementById('nextButton');
+    const formSteps = Array.from(document.querySelectorAll('.formStep'));
+    const prevButton = document.getElementById('prevButton');
+    const nextButton = document.getElementById('nextButton');
 
-  const updateStep = (step) => {
-      formSteps.forEach((stepElement, index) => {
-          stepElement.classList.toggle('active', index === step - 1);
-          stepElement.classList.toggle('inactive', index !== currentStep - 1);
-      });
+    const updateStep = (step) => {
+        formSteps.forEach((stepElement, index) => {
+            stepElement.classList.toggle('active', index === step - 1);
+            stepElement.classList.toggle('inactive', index !== currentStep - 1);
+        });
 
-      prevButton.style.display = step > 1 ? 'inline-block' : 'none';
-      nextButton.textContent = step === totalSteps ? 'Cadastrar' : 'Próximo';
-  };
+        if (!!prevButton) {
+            prevButton.style.display = step > 1 ? 'inline-block' : 'none';
+        }
+        nextButton.textContent = step === totalSteps ? 'Cadastrar' : 'Próximo';
+    };
 
-  const validateStep = () => {
-      const activeStep = document.querySelector('.formStep.active');
-      const inputs = Array.from(activeStep.querySelectorAll('input[required]'));
+    const validateStep = () => {
+        const activeStep = document.querySelector('.formStep.active');
+        const inputs = Array.from(activeStep.querySelectorAll('input[required]'));
 
-      for (const input of inputs) {
-          if (!input.value.trim()) {
-              const errorMessage = document.getElementById(`error-message`);
-              const errorText = document.getElementById(`error-text`);
-              errorText.textContent = 'Preencha todos os campos obrigatórios.';
-              errorMessage.style.display = 'block';
-              return false;
-          }
-      }
+        for (const input of inputs) {
+            if (!input.value.trim()) {
+                const errorMessage = document.getElementById(`error-message`);
+                const errorText = document.getElementById(`error-text`);
+                errorText.textContent = 'Preencha todos os campos obrigatórios.';
+                errorMessage.style.display = 'block';
+                return false;
+            }
+        }
 
-      return true;
-  };
+        return true;
+    };
 
-  nextButton.addEventListener('click', () => {
-      if (currentStep === totalSteps) {
-          alert('Cadastro concluído!');
-          changeSitePage('views/tela_jogos.html', null)
-          return;
-      }
+    nextButton.addEventListener('click', () => {
+        if (currentStep === totalSteps) {
+            if (type === 'login') {
+                console.log('Logado!')
+                sessionStorage.setItem('logged', true);
+                requestReloadTo('/views/tela_jogos');
+            }
+            changeSitePage('views/tela_jogos.html', null)
+            return;
+        }
 
-      if (validateStep()) {
-          currentStep++;
-          updateStep(currentStep);
-      }
-  });
+        if (validateStep()) {
+            currentStep++;
+            updateStep(currentStep);
+        }
+    });
 
-  prevButton.addEventListener('click', () => {
-      currentStep--;
-      updateStep(currentStep);
-  });
 
-  updateStep(currentStep);
+    if (!!prevButton) {
+        prevButton.addEventListener('click', () => {
+            currentStep--;
+            updateStep(currentStep);
+        });
+    }
+
+    updateStep(currentStep);
 
 });
--- js/homePage.js
+++ js/homePage.js
@@ -4,6 +4,7 @@ const hideNavigator = document.getElementById('hideNavigator');
 const header = document.getElementById('menu-header');
 const overlay = document.getElementById('site_overlay');
 const loading = document.getElementById('site_loading');
+const elementsLogged = document.querySelectorAll('#reqLog');
 
 function changeSitePage(src, title, focus = null, hide = null) {
 
@@ -22,7 +23,7 @@ function changeSitePage(src, title, focus = null, hide = null) {
     } else {
         navigator.classList.remove('hide');
     }
-    
+
     var iframe = document.getElementById('site_page');
     iframe.src = src;
     iframe.title = title;
@@ -34,17 +35,21 @@ function changeSitePage(src, title, focus = null, hide = null) {
         script.type = 'text/javascript';
         script.text = `
             function changeSitePage(src, title, focus = false, hide = false) {
-                window.parent.postMessage(
-                { 
+                window.parent.postMessage({ 
                     action: 'changeSitePage',
                     src: src, 
                     title: title, 
                     focus: focus,
                     hide: hide
-                },
-                '*'
-            );
+                },'*');
                 console.log("Solicitação enviada!");
+            }
+
+            function requestReloadTo(path) {
+                window.parent.postMessage({ 
+                    action: 'requestReload',
+                    path: path
+                },'*');
             }
         `;
         iframe.contentDocument.body.appendChild(script);
@@ -74,20 +79,39 @@ function changeSiteModal(src, close = false) {
             `;
             iframe.contentDocument.body.appendChild(script);
         };
-    
+
     } else {
         overlay.classList.add('hidden');
         iframe.src = '';
     }
 }
 
+function logoutUser() {
+    sessionStorage.removeItem('logged');
+    window.location.reload();
+}
+
+function requestReload(path) {
+    if (!!path) {
+        changeSiteModal(path)
+    }
+    window.location.reload();
+}
+
 window.addEventListener('message', function (event) {
     console.log("Mensagem recebida na página principal:", event.data);
+    console.log(`Função "${event.data.action}" requisitada...`);
     if (event.data.action === 'changeSitePage') {
         changeSitePage(event.data.src, event.data.title, event.data.focus, event.data.hide);
 
     } else if (event.data.action === 'changeSiteModal') {
         changeSiteModal(event.data.src,  event.data.close);
+
+    } else if (event.data.action === 'requestReload') {
+        requestReload();
+
+    } else {
+        console.warn(`Função "${event.data.action}" desconhecida!`);
     }
 });
 
@@ -96,3 +120,46 @@ hideNavigator.addEventListener('click', () => {
 });
 
 changeSitePage('views/tela_jogos.html', 'Início');
+
+const isLogged = sessionStorage.getItem("logged") === 'true';
+
+if (!isLogged) {
+
+    elementsLogged.forEach(element => {
+        element.style.display = 'none';
+    });
+
+    // Cria o elemento de Login
+    let frame = document.createElement('li');
+    let icon = document.createElement('div');
+    icon.textContent = "login";
+    icon.className = "material-symbols-outlined";
+    let text = document.createElement('p');
+    text.textContent = "Login";
+    frame.appendChild(icon);
+    frame.appendChild(text);
+    frame.addEventListener('click', function () {
+        changeSitePage('views/tela_login.html?type=login', 'Logar', true, true);
+    });
+
+    // Adiciona o elemento de Login à lista
+    let userActionsUl = document.querySelector('#user-actions-ul');
+    userActionsUl.appendChild(frame);
+} else {
+    // Cria o elemento de Sair (apenas se o usuário estiver logado)
+    let frame = document.createElement('li');
+    let icon = document.createElement('div');
+    icon.textContent = "logout";
+    icon.className = "material-symbols-outlined";
+    let text = document.createElement('p');
+    text.textContent = "Sair";
+    frame.appendChild(icon);
+    frame.appendChild(text);
+    frame.addEventListener('click', function () {
+        logoutUser();
+    });
+
+    // Adiciona o elemento de Sair à lista
+    let userActionsUl = document.querySelector('#user-actions-ul');
+    userActionsUl.appendChild(frame);
+}
